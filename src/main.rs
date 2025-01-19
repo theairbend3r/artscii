03d491c349426aa2a481d449d5c36dd7f226a59d
Akshaj Verma <akshajverma7@gmail.com>
2025-01-19 17:22:04 +0100

    Create canvas with terminal size.

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,63 @@ const ASCII: [&str; 14] = [
     "@", "&", "#", "$", "*", "+", "|", "^", "-", ";", ":", "'", ",", ".",
 ];
 
+#[derive(Debug)]
 struct Canvas {
     width: usize,
     height: usize,
+    aspect_ratio: f32,
 }
 
-struct Ascii {
-    width: usize,
-    height: usize,
+impl Canvas {
+    fn new() -> Self {
+        let terminal_size = crossterm::terminal::size();
+
+        match terminal_size {
+            Ok((width, height)) => {
+                let width: usize = width as usize;
+                let height: usize = height as usize;
+                let aspect_ratio: f32 = width as f32 / height as f32;
+
+                Canvas {
+                    width,
+                    height,
+                    aspect_ratio,
+                }
+            }
+            Err(_) => {
+                eprintln!("Creating with default height=80 and width=24.");
+                Canvas {
+                    width: 80,
+                    height: 24,
+                    aspect_ratio: (80 / 24) as f32,
+                }
+            }
+        }
+    }
 }
 
-struct Image {
-    path: PathBuf,
-    width: usize,
-    height: usize,
-}
-
-fn map_pixel_to_ascii(pixel: &Luma<u8>) -> String {
-    let Luma([luma_value]) = *pixel;
-    let px: usize = luma_value as usize;
-    let idx: usize = (px * (ASCII.len() - 1)) / 255;
-    ASCII[idx].to_string()
-}
+// struct Ascii {
+//     width: usize,
+//     height: usize,
+// }
+//
+// struct Image {
+//     path: PathBuf,
+//     width: usize,
+//     height: usize,
+// }
+//
+// fn map_pixel_to_ascii(pixel: &Luma<u8>) -> String {
+//     let Luma([luma_value]) = *pixel;
+//     let px: usize = luma_value as usize;
+//     let idx: usize = (px * (ASCII.len() - 1)) / 255;
+//     ASCII[idx].to_string()
+// }
 
 fn main() {
+    let canvas = Canvas::new();
+    println!("{:?}", canvas);
+
     // let rgb = open("./../spaceship.png").unwrap();
     //
     // let new_height: usize = 32;
