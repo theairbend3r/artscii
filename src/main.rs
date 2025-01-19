55d111c4de9207cbdfc7651b351a2e12bc845371
Akshaj Verma <akshajverma7@gmail.com>
2025-01-19 16:42:00 +0100

    Create structs.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+use std::path::PathBuf;
+
 use image::{imageops::FilterType, open, Luma};
 
 const ASCII: [&str; 14] = [
     "@", "&", "#", "$", "*", "+", "|", "^", "-", ";", ":", "'", ",", ".",
 ];
+
+struct Canvas {
+    width: usize,
+    height: usize,
+}
+
+struct Ascii {
+    width: usize,
+    height: usize,
+}
+
+struct Image {
+    path: PathBuf,
+    width: usize,
+    height: usize,
+}
 
 fn map_pixel_to_ascii(pixel: &Luma<u8>) -> String {
     let Luma([luma_value]) = *pixel;
@@ -12,26 +30,26 @@ fn map_pixel_to_ascii(pixel: &Luma<u8>) -> String {
 }
 
 fn main() {
-    let rgb = open("./../spaceship.png").unwrap();
-
-    let new_height: usize = 32;
-    let new_width: usize = 32;
-    let rgb_resized = rgb.resize(
-        new_width.try_into().unwrap(),
-        new_height.try_into().unwrap(),
-        FilterType::Nearest,
-    );
-    let gray = rgb_resized.into_luma8();
-    println!("{:?}", gray);
-    println!("{:?}", gray.dimensions());
-
-    let mut ascii_img = vec![];
-    for (_, _, px) in gray.enumerate_pixels() {
-        let ascii_val = map_pixel_to_ascii(px);
-        ascii_img.push(ascii_val);
-    }
-
-    for chunk in ascii_img.chunks(new_width) {
-        println!("{:?}", chunk.join(" "));
-    }
+    // let rgb = open("./../spaceship.png").unwrap();
+    //
+    // let new_height: usize = 32;
+    // let new_width: usize = 32;
+    // let rgb_resized = rgb.resize(
+    //     new_width.try_into().unwrap(),
+    //     new_height.try_into().unwrap(),
+    //     FilterType::Nearest,
+    // );
+    // let gray = rgb_resized.into_luma8();
+    // println!("{:?}", gray);
+    // println!("{:?}", gray.dimensions());
+    //
+    // let mut ascii_img = vec![];
+    // for (_, _, px) in gray.enumerate_pixels() {
+    //     let ascii_val = map_pixel_to_ascii(px);
+    //     ascii_img.push(ascii_val);
+    // }
+    //
+    // for chunk in ascii_img.chunks(new_width) {
+    //     println!("{:?}", chunk.join(" "));
+    // }
 }
