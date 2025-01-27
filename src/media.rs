ba1841fc75d3d12c9486af527d3b07e5acdd54a9
Akshaj Verma <akshajverma7@gmail.com>
2025-01-27 17:23:40 +0100

    Rename img to media.

--- src/media.rs
+++ src/media.rs
@@ -2,25 +2,40 @@ use image::{imageops::FilterType, DynamicImage, ImageBuffer, Luma};
 use std::path::PathBuf;
 
 #[derive(Debug)]
-pub struct Img {
+enum MediaType {
+    Image,
+    Video,
+}
+
+#[derive(Debug)]
+enum MediaColour {
+    Gray,
+    Colour,
+}
+
+#[derive(Debug)]
+pub struct Media {
     path: PathBuf,
     pub width: u32,
     pub height: u32,
     pub aspect_ratio: f32,
-    img: DynamicImage,
+    media_type: MediaType,
+    media_colour: MediaColour,
+    frames: Vec<DynamicImage>, // vector will have only 1 item if it's an image
 }
 
-impl Img {
+impl Media {
     pub fn new(path: PathBuf) -> Self {
-        let img = image::open(&path).expect("Image not found.");
-        let aspect_ratio = img.width() as f32 / img.height() as f32;
+        let img = image::open(&path).expect("Media not found.");
 
-        Img {
+        Media {
             path,
             width: img.width(),
             height: img.height(),
-            aspect_ratio,
-            img,
+            aspect_ratio: img.width() as f32 / img.height() as f32,
+            media_type: MediaType::Image,
+            media_colour: MediaColour::Colour,
+            frames: vec![img],
         }
     }
 
@@ -32,11 +47,11 @@ impl Img {
         resize_filter: FilterType,
     ) -> ImageBuffer<Luma<u8>, Vec<u8>> {
         if preserve_aspect_ratio {
-            self.img
+            self.frames[0]
                 .resize(new_width, new_height, resize_filter)
                 .into_luma8()
         } else {
-            self.img
+            self.frames[0]
                 .resize_exact(new_width, new_height, resize_filter)
                 .into_luma8()
         }
