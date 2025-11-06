e48b1c5abd87aa10465cfae7adeabe4aa81bd57c
Akshaj Verma <akshajverma7@gmail.com>
2025-11-06 23:41:23 +0100

    Add animation.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use image::{GenericImageView, ImageReader};
+use std::{thread, time::Duration};
 
 const ASCII_CHARS: &[char] = &['@', '#', '8', '&', 'o', ':', '*', '.', ' '];
 
 #[derive(Debug)]
 struct Frame {
-    arr: Vec<Vec<u8>>,
+    data: Vec<Vec<u8>>,
+}
+
+#[derive(Debug)]
+struct Frames {
+    data: Vec<Frame>,
 }
 
 impl Frame {
@@ -27,7 +33,7 @@ impl Frame {
             }
         }
 
-        Self { arr }
+        Self { data: arr }
     }
 
     fn convert_to_ascii(pixel: u8) -> char {
@@ -36,18 +42,42 @@ impl Frame {
     }
 
     fn render(self) {
-        for row in self.arr {
+        for row in self.data {
             for col in row {
-                print!("{}", Self::convert_to_ascii(col));
+                // print!("{}", Self::convert_to_ascii(col));
+                print!("{:4?}", col);
             }
             println!()
         }
     }
 }
 
-fn main() {
-    let file_path = "./../10002.png";
-    let frame = Frame::new(file_path);
+impl Frames {
+    fn render(self) {
+        for frame in self.data {
+            frame.render();
+            print!("\x1b[{}D", 28);
+            print!("\x1b[{}A", 14);
 
-    frame.render();
+            thread::sleep(Duration::from_secs(1));
+        }
+    }
+}
+
+fn main() {
+    let file_paths = vec![
+        "./../1.png",
+        "./../10006.png",
+        "./../10009.png",
+        "./../10002.png",
+    ];
+
+    let mut frames = Frames { data: vec![] };
+
+    for fp in file_paths {
+        let frame = Frame::new(fp);
+        frames.data.push(frame)
+    }
+
+    frames.render();
 }
