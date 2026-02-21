47cd625353a0903106ad4b10e003900173f90ba8
Akshaj Verma <akshajverma7@gmail.com>
2026-02-21 17:40:26 +0100

    Create a frame struct for image proc.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod frame;
+
+use frame::Frame;
+
 use clap::Parser;
 use std::path::PathBuf;
 
@@ -9,47 +13,8 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
-    let ascii_chars = "@%#*+=-:. ";
-
-    let img = image::open(&args.path).expect("Failed to open image.");
-
-    //  terminal characters are approx twice as high as they are wide
-    let img_resized = img.resize(
-        img.width(),
-        img.width() / 2,
-        image::imageops::FilterType::Nearest,
-    );
-
-    // let img_gray = img_resized.grayscale();
-    let img_gray = img_resized.to_luma8();
-
     println!("Path provided: {:?}", args.path);
-    println!("Ascii chars: {:?}", ascii_chars);
-    println!("Loaded image: {}x{}", img.width(), img.height());
-    println!(
-        "Resized image: {}x{}",
-        img_resized.width(),
-        img_resized.height()
-    );
-    println!(
-        "Grayscale image: {}x{}",
-        img_gray.width(),
-        img_gray.height()
-    );
 
-    for y in 0..img_gray.height() {
-        for x in 0..img_gray.width() {
-            let pixel = img_gray.get_pixel(x, y);
-            let brightness = pixel[0] as usize;
-
-            // multiply first and divide later because in rust
-            // dividing two integers results in an integer which throws away the decimal.
-            let ascii_idx = brightness * (ascii_chars.len() - 1) / 255;
-            let ascii_char = ascii_chars.chars().nth(ascii_idx).unwrap();
-
-            print!("{}", ascii_char);
-        }
-        println!();
-    }
+    let frame = Frame::from_path(&args.path);
+    frame.render();
 }
