3c686deebea9c1d8121dc1426f10f17f743ed59f
Akshaj Verma <akshajverma7@gmail.com>
2026-02-21 16:22:35 +0100

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,21 @@
+mod frame;
+mod frames;
+
+use crate::frame::Frame;
+use crate::frames::Frames;
+
 use std::{
     fs::read_dir,
     path::{Path, PathBuf},
-    thread,
-    time::Duration,
 };
 
 use clap::Parser;
-use image::{DynamicImage, GenericImageView, ImageReader};
-
-const ASCII_CHARS: &[char] = &['@', '#', '8', '&', 'o', ':', '*', '.', ' '];
-
-#[derive(Debug)]
-struct Frame {
-    data: Vec<Vec<u8>>,
-}
-
-#[derive(Debug)]
-struct Frames {
-    data: Vec<Frame>,
-}
+use image::{DynamicImage, ImageReader};
 
 fn read_image(file_path: &PathBuf) -> DynamicImage {
     let img_reader = ImageReader::open(file_path).expect("No file found.");
     img_reader.decode().expect("Could not open image.")
 }
-
-impl Frame {
-    fn from_image(img: DynamicImage) -> Self {
-        let (width, height) = img.dimensions();
-
-        let img = img
-            .resize_exact(width, height / 2, image::imageops::FilterType::Nearest)
-            .to_luma8();
-
-        let (width, height) = img.dimensions();
-
-        let mut arr: Vec<Vec<u8>> = vec![vec![0; width as usize]; height as usize];
-
-        for y in 0..img.height() {
-            for x in 0..img.width() {
-                arr[y as usize][x as usize] = img.get_pixel(x, y).0[0];
-            }
-        }
-
-        Self { data: arr }
-    }
-
-    fn pixel_to_ascii(pixel: u8) -> char {
-        let ascii_idx = pixel as usize * (ASCII_CHARS.len() - 1) / 255;
-        ASCII_CHARS[ascii_idx]
-    }
-
-    fn render(self) {
-        for row in self.data {
-            for col in row {
-                print!("{}", Self::pixel_to_ascii(col));
-                // print!("{:4?}", col);
-            }
-            println!()
-        }
-    }
-}
-
-impl Frames {
-    fn render(self) {
-        for frame in self.data {
-            frame.render();
-            print!("\x1b[{}D", 28);
-            print!("\x1b[{}A", 14);
-
-            thread::sleep(Duration::from_secs(1));
-        }
-    }
-}
-
 fn is_image_file(path: &Path) -> bool {
     match path.extension() {
         Some(ext) => ["png", "jpg", "jpeg"].contains(&ext.to_str().unwrap()),
@@ -81,7 +23,7 @@ fn is_image_file(path: &Path) -> bool {
     }
 }
 
-/// path to image to display or directory of images to animate"
+/// path to image to display or directory of images to animate
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long, help = "path to file or dir.")]
