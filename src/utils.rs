867fc992ad3a959cb9dd63d715f22fb85fcefcc7
Akshaj Verma <akshajverma7@gmail.com>
2026-03-22 15:29:12 +0100

    Add logging.

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,19 @@
 use crossterm::terminal;
+use log::{info, warn};
 
 pub fn get_terminal_size() -> (u32, u32) {
     match terminal::size() {
-        Ok((w, h)) => (w as u32, h as u32),
-        Err(_) => (80, 24), // fallback
+        Ok((w, h)) => {
+            info!("Detected terminal size: {}x{}", w, h);
+            (w as u32, h as u32)
+        }
+        Err(e) => {
+            warn!(
+                "Unable to get terminal size ({}). Using the default size of (80,24)",
+                e
+            );
+            (80, 24)
+        } // fallback
     }
 }
 
