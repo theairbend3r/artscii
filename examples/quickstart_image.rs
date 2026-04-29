8e7d6d8a0de0c38de2995cbdc03bee8723b36347
Akshaj Verma <akshajverma7@gmail.com>
2026-04-29 22:31:19 +0200

    Update image example.

--- examples/quickstart_image.rs
+++ examples/quickstart_image.rs
@@ -8,13 +8,8 @@ use artscii::core::{
 fn main() {
     // load image from disk into a Frame
     let path = PathBuf::from("./../test-images/cuddlyferris.png");
-    let img_decoder = ImageDecoder::new(path);
-    let frame = img_decoder
-        .decode()
-        .unwrap()
-        .resize(40, 20)
-        .to_ascii()
-        .unwrap();
+    let img_decoder = ImageDecoder::new(path).decode().unwrap();
+    let frame = img_decoder.resize(40, 20).to_ascii().unwrap();
 
     // optionally load canvas to print
     let canvas = Canvas::new(210, 53);
