d1cd6011003d64232c715bc7f0aff05760f4a9e9
Akshaj Verma <akshajverma7@gmail.com>
2026-04-29 21:27:49 +0200

    First pass at multi-frame iterator implementation.

--- examples/gif.rs
+++ examples/gif.rs
@@ -1,7 +1,7 @@
 use artscii::core::canvas::{Canvas, Padding};
 use artscii::core::frame::Frame;
 use image::codecs::gif::GifDecoder;
-use image::{AnimationDecoder, DynamicImage, ImageDecoder};
+use image::{AnimationDecoder, DynamicImage};
 use std::fs::File;
 use std::io::BufReader;
 use std::{
@@ -10,26 +10,55 @@ use std::{
     time::Duration,
 };
 
+struct FrameIter<'a> {
+    inner: image::Frames<'a>,
+}
+
+impl FrameIter<'_> {
+    fn new() -> Self {
+        let file = File::open("./../test-images/countdown.gif").unwrap();
+        let reader = BufReader::new(file);
+        let decoder = GifDecoder::new(reader).unwrap();
+
+        let gif_frames = decoder.into_frames();
+
+        Self { inner: gif_frames }
+    }
+}
+
+impl Iterator for FrameIter<'_> {
+    type Item = Frame;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let nextframe = self.inner.next().unwrap();
+        let buffer = nextframe.unwrap().into_buffer();
+
+        let (width, height) = buffer.dimensions();
+
+        let gray = DynamicImage::ImageRgba8(buffer).to_luma8();
+        let pixels = gray.into_raw();
+        let frame = Frame::new(pixels, width, height);
+
+        // let mut gray = Vec::new();
+        // for pixel in buffer.pixels() {
+        //     let [r, g, b, _a] = pixel.0;
+        //     let y = (0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32) as u8;
+        //     gray.push(y)
+        // }
+        //
+        // let frame = Frame::new(gray, width, height);
+
+        Some(frame)
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file = File::open("./../test-images/countdown.gif").unwrap();
-    let reader = BufReader::new(file);
-
-    let decoder = GifDecoder::new(reader).unwrap();
-    let (width, height) = decoder.dimensions();
-    println!("{} {}", width, height);
-
-    let frames = decoder.into_frames();
+    let gif = FrameIter::new();
     let canvas = Canvas::new(210, 53);
 
-    for frame in frames {
-        let frame = frame?;
-
-        let gray = DynamicImage::ImageRgba8(frame.into_buffer()).to_luma8();
-        let pixels = gray.into_raw();
-        let f = Frame::new(pixels, width, height)
-            .resize(80, 40)
-            .to_ascii()
-            .unwrap();
+    for f in gif {
+        // println!("{:?}, {:?}", f.pixels.len(), &f.pixels[..5]);
+        let f = f.resize(80, 40).to_ascii().unwrap();
 
         // clear screen + move cursor to top-left
         print!("\x1b[2J\x1b[H");
