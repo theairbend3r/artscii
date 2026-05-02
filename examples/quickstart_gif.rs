54b74ac2d7c17a8bd6596177f2e6ca2f160f3f50
Akshaj Verma <akshajverma7@gmail.com>
2026-05-02 17:38:50 +0200

    Update examples.

--- examples/quickstart_gif.rs
+++ examples/quickstart_gif.rs
@@ -9,16 +9,16 @@ use std::{
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = PathBuf::from("./../test-images/countdown.gif");
-    let gif = ReaderGif::new(path);
+    let gif_iter = ReaderGif::new(path);
     let canvas = Canvas::new(210, 53);
 
-    for f in gif {
-        let f = f.resize(80, 40).to_ascii().unwrap();
+    for frame in gif_iter {
+        let frame = frame.resize(80, 40).to_ascii()?;
 
         // clear screen + move cursor to top-left
         print!("\x1b[2J\x1b[H");
-        canvas.render(f, Padding::Center);
-        io::stdout().flush().unwrap();
+        canvas.render(frame, Padding::Center);
+        io::stdout().flush()?;
         thread::sleep(Duration::from_millis(50));
     }
 
--- examples/quickstart_image.rs
+++ examples/quickstart_image.rs
@@ -5,14 +5,16 @@ use artscii::core::{
     reader::image::ReaderImage,
 };
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // load image from disk into a Frame
     let path = PathBuf::from("./../test-images/cuddlyferris.png");
-    let img_decoder = ReaderImage::new(path);
-    let frame = img_decoder.decode().unwrap();
-    let frame = frame.resize(40, 20).to_ascii().unwrap();
-
-    // optionally load canvas to print
+    let reader = ReaderImage::new(path);
     let canvas = Canvas::new(210, 53);
+
+    let frame = reader.read()?;
+    let frame = frame.resize(40, 20).to_ascii()?;
+
     canvas.render(frame, Padding::Center);
+
+    Ok(())
 }
