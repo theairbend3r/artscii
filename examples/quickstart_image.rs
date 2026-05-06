3402e1301d159f6ae272dc1c5aec1281d87006c7
Akshaj Verma <akshajverma7@gmail.com>
2026-05-06 21:38:36 +0200

    Update example.

--- examples/quickstart_image.rs
+++ examples/quickstart_image.rs
@@ -6,28 +6,6 @@ use artscii::core::{
     reader::image::ReaderImage,
 };
 
-fn rgb_to_ansi(r: u8, g: u8, b: u8, threshold: u8) -> u8 {
-    let r_bit = if r > threshold { 1 } else { 0 };
-    let g_bit = if g > threshold { 1 } else { 0 };
-    let b_bit = if b > threshold { 1 } else { 0 };
-
-    let rgb_value = (r_bit << 2) | (g_bit << 1) | b_bit;
-
-    let ansi_index = match rgb_value {
-        0 => 0, // black
-        1 => 4, // blue
-        2 => 2, // green
-        3 => 6, // cyan
-        4 => 1, // red
-        5 => 5, // magenta
-        6 => 3, // yellow
-        7 => 7, // white
-        _ => 0,
-    };
-
-    30 + ansi_index
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // load image from disk into a Frame
     let path = PathBuf::from("./../test-images/cuddlyferris.png");
